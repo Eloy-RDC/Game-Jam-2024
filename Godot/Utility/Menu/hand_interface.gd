197093e8f594e754ab981b1b595c648bb9c8c90c
ktspanther <78915950+ktspanther@users.noreply.github.com>
2024-03-23 14:38:23 +0100

    Start HandInterface

---
+++ Godot/Utility/Menu/hand_interface.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+
+@onready var Card1 = %Card1
+@onready var Card2 = %Card2
+@onready var Card3 = %Card3
+
+
+
+@onready var Icon_list = [preload("res://Texture/YellowSquare.png")]
+
+func _ready():
+	Card1.visible = true
+	Card1.icon = Icon_list[0]
+
+
+func _on_card_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_card_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_card_3_pressed():
+	pass # Replace with function body.
