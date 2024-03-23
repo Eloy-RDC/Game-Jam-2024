b39ea34b12632448a376e1553b5113a90b2039e5
Eloy-RDC <eloy.rdc@gmail.com>
2024-03-23 21:03:28 +0100

    les buttons fonctionnent mdr

--- Godot/Utility/Menu/hand_interface.gd
+++ Godot/Utility/Menu/hand_interface.gd
@@ -1,27 +1,30 @@
 extends Control
 
+@onready var lib = get_node("../../Library").get_children()
+@onready var inventory = [0, 1, 2, 3, 4, 5]
+@onready var deck = [0, 1, 2, 3, 4, 5]
+@onready var hand = []
+@onready var handCards = []
+@onready var buttons = [$HandCard1, $HandCard2, $HandCard3]
 
-@onready var Icon_list = [preload("res://Texture/YellowSquare.png")]
-
-@onready var lib = []
-
-var inventory = []
-
-var deck = []
-
-var hand = []
 
 func _ready():
-	pass
+	deck.shuffle()
+	for i in range(3):
+		handCards.append(lib[deck[i]])
+		buttons[i].texture_normal = handCards[i].get_node("Sprite2D").texture
 
 
-func _on_card_1_pressed():
-	print("Button 1 pressed")
+func _on_hand_card_1_pressed():
+	handCards[0].use()
+	print("Card 1 played")
 
 
-func _on_card_2_pressed():
-	print("Button 2 pressed")
+func _on_hand_card_2_pressed():
+	handCards[1].use()
+	print("Card 2 played")
 
 
-func _on_card_3_pressed():
-	print("Button 3 pressed")
+func _on_hand_card_3_pressed():
+	handCards[2].use()
+	print("Card 3 played")
--- Godot/Utility/player/player.gd
+++ Godot/Utility/player/player.gd
@@ -9,3 +9,10 @@ func are_nextturn_shield():
 	if nextturn_shield > 0:
 		shield += nextturn_shield
 		nextturn_shield = 0
+
+
+# turn based combat
+func battle():
+	var victory = 0 # 1 = player wins; -1 = player loses
+	while victory == 0:
+		pass
